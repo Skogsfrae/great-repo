eb504ee7d7706393d17aa45bd0bfcd5185e8a8f5
Skogsfrae <skogafrae@gmail.com>
2014-02-21 16:14:23 +0100

    Cartelle eliminate

--- h/libuarm.h
+++
@@ -1,8 +0,0 @@
-#ifndef UARM_LIBUARM_H
-#define UARM_LIBUARM_H
-
-void tprint(const char *s);
-
-extern unsigned int LDST(void *addr);
-
-#endif //UARM_LIBURAM_H
--- h/types.h
+++
@@ -1,7 +0,0 @@
-#ifndef TYPES_H
-#define TYPES_H
-
-/* temporary definition for phase1 */
-typedef void *state_t;
-
-#endif
